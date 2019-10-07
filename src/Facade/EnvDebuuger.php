4af2c3874f1ab81f7e19e9bdb52b0cbf7a39f48a
Salman Zafar <salman.zafar@innowi.com>
2019-10-07 18:03:13 +0500

    working on facade

---
+++ src/Facade/EnvDebuuger.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Salman\DbBackup\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class EnvDebuuger extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'EnvDebuuger';
+    }
+
+}
